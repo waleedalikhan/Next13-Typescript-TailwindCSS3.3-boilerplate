0d896d744e43e10e56d30872812e1935905c2eb4
Waleed Ali Khan <waleedomore@gmail.com>
2023-04-09 20:21:11 +0500

    Next13 Typescript and TailwindCSS3.3 starter boilerplate

---
+++ src/helpers/AppConfig.ts
@@ -0,0 +1,6 @@
+export const AppConfig = {
+  site_name: 'Fortuna Y Poder',
+  title: 'A News Website',
+  description: 'This website is used for social and news media purposes',
+  locale: 'en',
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from 'next'
+
+const HomePage: NextPage = () => {
+  return (
+    <>
+      <h1 className="text-5xl text-red-500">Hello World!</h1>
+    </>
+  )
+}
+
+export default HomePage
